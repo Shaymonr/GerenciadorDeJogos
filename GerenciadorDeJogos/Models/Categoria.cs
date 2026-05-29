741bbddc299f86b720cc60df14603dc3ee80ed55
SaymonB <saymonr.contato@gmail.com>
2026-05-29 16:48:09 -0400

    Added Models
    Added Models "Categoria" and "Jogo"

---
+++ GerenciadorDeJogos/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿namespace GerenciadorDeJogos.Models
+{
+    public class Categoria
+    {
+        // Propriedades do modelo Categoria
+        public int CategoriaId { get; set; }
+    
+        public string CategoriaNome { get; set; }
+
+        // Propriedade de navegação para os jogos relacionados a esta categoria
+        public List<Jogo> Jogos { get; set; }
+
+
+    }
+}
+
---
+++ GerenciadorDeJogos/Models/Jogo.cs
@@ -0,0 +1,24 @@
+﻿namespace GerenciadorDeJogos.Models
+{
+    public class Jogo
+    {
+        // Propriedades do modelo Jogo
+        public int JogoId { get; set; }
+
+        public string JogoNome { get; set; }
+
+        public string JogoDescricao { get; set; }
+
+        public string JogoImagem { get; set; }
+
+        public string JogoMiniatura { get; set; }
+        
+        public bool JogoConcluido { get; set; }
+
+        // Chave estrangeira para Categoria
+        public int CategoriaId { get; set; }
+        // Propriedade de navegação para Categoria
+        public virtual Categoria Categoria { get; set; }
+
+    }
+}
